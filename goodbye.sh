40fe026a6153a29082f638b3526947833d1d8181
rusliktest <rusliksemp@gmail.com>
2021-07-26 17:54:10 +0300

    release v1.4

--- goodbye.sh
+++ goodbye.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 VERSION="v1.4"
 
-git add . & git commit -m "release ${VERSION}" & git tag -a -m "My action release ${VERSION}" ${VERSION} & git push origin main --follow-tags
+git add . & 
+git commit -m "release ${VERSION}" & 
+git tag -a -m "My action release ${VERSION}" ${VERSION} & 
+git push origin main --follow-tags
